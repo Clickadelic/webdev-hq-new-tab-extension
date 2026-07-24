7a46a6e73a4f3280f0514d2526af8a8bdc1817ca
Clickadelic <toby.hopp@gmail.com>
2026-07-25 00:04:49 +0200

    ThemeProvider

--- entrypoints/newtab/App.tsx
+++ entrypoints/newtab/App.tsx
@@ -10,7 +10,8 @@ import { useState } from "react";
 import { BsTextIndentRight } from "react-icons/bs";
 import { BsTextIndentLeft } from "react-icons/bs";
 import { HiMenuAlt3 } from "react-icons/hi";
-
+import { ThemeProvider } from "@/components/global/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 import { Button } from "@/components/ui/button";
 import { AccountDropdown } from "@/components/dashboard/AccountDropdown";
 import { SidebarSheet } from "@/components/dashboard/SidebarSheet";
@@ -80,66 +81,68 @@ const App = () => {
 		setSidebarOpen(!isSidebarOpen);
 	};
 	return (
-		<BackgroundImage creditsPosition="center">
-			<SidebarProvider>
-				<aside className={cn("App-sidebar hidden fixed md:block top-0 left-0 min-h-screen bg-white")}>
-					<Sidebar collapsible="icon">
-						<SidebarHeader className="flex items-center justify-center p-0">
-							<Logo url={`${import.meta.env.WXT_HOMEPAGE_URL}`} isSidebarOpen={isSidebarOpen} />
-						</SidebarHeader>
-						<SidebarContent className="pt-8">
-							<NavMain items={data.navMain} />
-							<NavCommunityItems communityItems={data.communityItems} />
-						</SidebarContent>
-						<SidebarFooter>
-							<NavUser user={data.user} />
-						</SidebarFooter>
-						<SidebarRail />
-					</Sidebar>
-				</aside>
-				<header className={cn("App-header flex fixed top-0 md:ml-64 w-screen h-15 p-3 border-b bg-white z-50")}>
-					<nav className="header-nav flex justify-between w-max">
-						<div className="inline-flex gap-3">
-							<SidebarTrigger variant="ghost" size="sm" className="rounded-xs">
-								<BsTextIndentRight className="text-2xl" />
-							</SidebarTrigger>
-							<form>
-								<input className="p-2 border rounded w-72 border-black" type="search" placeholder="Search" />
-							</form>
-						</div>
-						<ul className="hidden md:space-x-1 md:fixed md:right-5 md:mt-.5 md:inline-flex md:mr-1 mb-1">
-							<li>
-								<MegaMenu />
-							</li>
-							<li>
-								<LangSwitch />
-							</li>
-							<li>
-								<NotificationButton />
-							</li>
-							<li>
-								<InboxButton />
-							</li>
-							<li>
-								<FullscreenButton />
-							</li>
-							<li>
-								<SidebarSheet />
-							</li>
-							<li>
-								<AccountDropdown />
-							</li>
-						</ul>
-					</nav>
-				</header>
-				<main className="container mx-auto mt-32">
-					<Routes>
-						{/* Hashrouter is ready, just add routes */}
-						<Route path="/" element={<DashboardPage />} />
-					</Routes>
-				</main>
-			</SidebarProvider>
-		</BackgroundImage>
+		<ThemeProvider>
+			<BackgroundImage creditsPosition="center">
+				<SidebarProvider>
+					<aside className={cn("App-sidebar hidden fixed md:block top-0 left-0 min-h-screen bg-white")}>
+						<Sidebar collapsible="icon">
+							<SidebarHeader className="flex items-center justify-center p-0">
+								<Logo url={`${import.meta.env.WXT_HOMEPAGE_URL}`} isSidebarOpen={isSidebarOpen} />
+							</SidebarHeader>
+							<SidebarContent className="pt-8">
+								<NavMain items={data.navMain} />
+								<NavCommunityItems communityItems={data.communityItems} />
+							</SidebarContent>
+							<SidebarFooter>
+								<NavUser user={data.user} />
+							</SidebarFooter>
+							<SidebarRail />
+						</Sidebar>
+					</aside>
+					<header className={cn("App-header flex fixed top-0 md:ml-64 w-screen h-15 p-3 border-b bg-white z-50")}>
+						<nav className="header-nav flex justify-between w-max">
+							<div className="inline-flex gap-3">
+								<SidebarTrigger variant="ghost" size="sm" className="rounded-xs">
+									<BsTextIndentRight className="text-2xl" />
+								</SidebarTrigger>
+								<form>
+									<input className="p-2 border rounded w-72 border-black" type="search" placeholder="Search" />
+								</form>
+							</div>
+							<ul className="hidden md:space-x-1 md:fixed md:right-5 md:mt-.5 md:inline-flex md:mr-1 mb-1">
+								<li>
+									<MegaMenu />
+								</li>
+								<li>
+									<LangSwitch />
+								</li>
+								<li>
+									<NotificationButton />
+								</li>
+								<li>
+									<InboxButton />
+								</li>
+								<li>
+									<FullscreenButton />
+								</li>
+								<li>
+									<SidebarSheet />
+								</li>
+								<li>
+									<AccountDropdown />
+								</li>
+							</ul>
+						</nav>
+					</header>
+					<main className="container mx-auto mt-32">
+						<Routes>
+							{/* Hashrouter is ready, just add routes */}
+							<Route path="/" element={<DashboardPage />} />
+						</Routes>
+					</main>
+				</SidebarProvider>
+			</BackgroundImage>
+		</ThemeProvider>
 	);
 };
 
