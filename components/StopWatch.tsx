8ac14a7dc125d6ec085f9349c50dad60808dfdf3
Clickadelic <toby.hopp@gmail.com>
2026-07-26 19:50:58 +0200

    text details stopwatch

--- components/StopWatch.tsx
+++ components/StopWatch.tsx
@@ -57,31 +57,31 @@ export default function StopWatch({ className }: StopWatchProps) {
 	return (
 		<div className={cn("w-64 bg-white/30 dark:bg-slate-800/30 flex flex-col items-center justify-center gap-2 p-1 rounded", className)}>
 			<div className="w-full bg-white dark:bg-slate-800 rounded p-3 flex flex-col gap-2 text-slate-800 dark:text-white">
-				<h2 className="text-lg flex gap-2 justify-center items-center">
+				<h2 className="text-lg flex gap-2 justify-center items-center cursor-default">
 					<LuClock />
 					{chrome.i18n.getMessage("timer", "Timer")}
 				</h2>
-				<h2 className="text-center text-lg">{`${time.hr < 10 ? 0 : ""}${time.hr} : ${time.min < 10 ? 0 : ""}${time.min} : ${time.sec < 10 ? 0 : ""}${time.sec}`}</h2>
+				<h2 className="text-center text-lg cursor-default">{`${time.hr < 10 ? 0 : ""}${time.hr} : ${time.min < 10 ? 0 : ""}${time.min} : ${time.sec < 10 ? 0 : ""}${time.sec}`}</h2>
 				<button
 					onClick={pauseOrResume}
-					className="flex items-center justify-center text-center hover:cursor-pointer"
+					className="text-lg flex items-center justify-center text-center hover:cursor-pointer hover:text-slate-500"
 					title={chrome.i18n.getMessage("start", "Start")}
 					aria-label={chrome.i18n.getMessage("start", "Start")}
 				>
 					{intervalId ? (
 						<div className="flex mx-auto gap-2">
-							<FaPause className="mt-0.5" />
+							<FaPause className="mt-1.5" />
 							{chrome.i18n.getMessage("pause", "Pause")}
 						</div>
 					) : (
 						<div className="flex mx-auto gap-2">
-							<FaPlay className="mt-0.5" />
+							<FaPlay className="mt-1.5" />
 							{chrome.i18n.getMessage("start", "Start")}
 						</div>
 					)}
 				</button>
 				<button
-					className="flex gap-2 items-center justify-center hover:cursor-pointer"
+					className="flex gap-2 items-center justify-center hover:cursor-pointer hover:text-slate-500"
 					onClick={reset}
 					title={chrome.i18n.getMessage("reset", "Reset")}
 					aria-label={chrome.i18n.getMessage("reset", "Reset")}
